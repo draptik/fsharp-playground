bd08d6c3df1c174b219ca4d17e0925dedec59c16
Patrick Drechsler <patrick@pdrechsler.de>
2017-06-29 20:52:28 +0200

    cleanup

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -2,27 +2,13 @@ module Bob
 
 open System
 
-// "s" can't be invered!
-// In this case we have to provide the type using the "(varName: type)" syntax
-// As C# developer I know that the .NET framework provides the extension method "foo".EndsWith("bla")
-//
-// This does not work
-//  let endsWithQuestionMark s =
-//      s.EndsWith "?"
-//
-// In the above example the F# compiler cannot "guess" the input type (string).
-// So we have to tell the compiler that this function only accepts strings using "(s:string)"
-//
 let endsWithQuestionMark (s:string) =
     s.EndsWith "?"
 
 let hey statement =
-
     if endsWithQuestionMark statement
         then "Sure."
     elif statement = "Tom-ay-to, tom-aaaah-to." 
         then "Whatever."
-    elif endsWithQuestionMark statement
-        then "Sure."
     else    
         "Whoa, chill out!"
