36e5921b18c0be39f066de477c324c1a2fc0b675
Patrick Drechsler <patrick@pdrechsler.de>
2017-06-29 21:16:38 +0200

    'Forceful question' test: green

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -2,11 +2,17 @@ module Bob
 
 open System
 
+let areAllWordsUpperCase (s:string) =
+    s = "WHAT THE HELL WERE YOU THINKING?"
+
 let endsWithQuestionMark (s:string) =
     s.EndsWith "?"
 
+
 let hey statement =
-    if endsWithQuestionMark statement
+    if areAllWordsUpperCase statement
+        then "Whoa, chill out!"
+    elif endsWithQuestionMark statement
         then "Sure."
     elif statement = "Tom-ay-to, tom-aaaah-to." 
         then "Whatever."
