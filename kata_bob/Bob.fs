32b05ed952824e09da6e56e56f1316296583845a
Patrick Drechsler <patrick@pdrechsler.de>
2017-06-29 22:02:51 +0200

    introduced isYelling function

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -5,12 +5,15 @@ open System
 let areAllWordsUpperCase (s:string) =
     s = "WHAT THE HELL WERE YOU THINKING?"
 
+let isYelling s =
+    areAllWordsUpperCase s
+
 let endsWithQuestionMark (s:string) =
     s.EndsWith "?"
 
 
 let hey statement =
-    if areAllWordsUpperCase statement then "Whoa, chill out!"
+    if isYelling statement then "Whoa, chill out!"
     elif endsWithQuestionMark statement then "Sure."
     elif statement = "Tom-ay-to, tom-aaaah-to." then "Whatever."
     elif statement = "1, 2, 3" then "Whatever."
