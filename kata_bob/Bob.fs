3a78bdc8e12df6cacea803a1e43942174c19b89e
Patrick Drechsler <patrick@pdrechsler.de>
2017-06-29 19:39:50 +0200

    asking question test: green

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -3,4 +3,7 @@ module Bob
 let hey statement =
     if statement = "Tom-ay-to, tom-aaaah-to." 
     then "Whatever."
-    else "Whoa, chill out!"
+    else
+        if statement = "Does this cryogenic chamber make me look fat?"
+        then "Sure."
+        else "Whoa, chill out!"
