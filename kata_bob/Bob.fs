94c268cdef25dc079727aeaf3c649aea8c4f5ea2
Patrick Drechsler <patrick@pdrechsler.de>
2017-06-29 19:44:51 +0200

    replaced nested if with elif

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -2,8 +2,8 @@ module Bob
 
 let hey statement =
     if statement = "Tom-ay-to, tom-aaaah-to." 
-    then "Whatever."
-    else
-        if statement = "Does this cryogenic chamber make me look fat?"
+        then "Whatever."
+    elif statement = "Does this cryogenic chamber make me look fat?"
         then "Sure."
-        else "Whoa, chill out!"
+    else    
+        "Whoa, chill out!"
