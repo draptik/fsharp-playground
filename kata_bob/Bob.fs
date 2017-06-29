7ad6bfddfb663dd440e11cf99a814f05b0462d81
Patrick Drechsler <patrick@pdrechsler.de>
2017-06-29 20:50:52 +0200

    refactored: extracted method 'endsWithQuestionMark' using .NET 'EndsWith'

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -1,9 +1,28 @@
 module Bob
 
+open System
+
+// "s" can't be invered!
+// In this case we have to provide the type using the "(varName: type)" syntax
+// As C# developer I know that the .NET framework provides the extension method "foo".EndsWith("bla")
+//
+// This does not work
+//  let endsWithQuestionMark s =
+//      s.EndsWith "?"
+//
+// In the above example the F# compiler cannot "guess" the input type (string).
+// So we have to tell the compiler that this function only accepts strings using "(s:string)"
+//
+let endsWithQuestionMark (s:string) =
+    s.EndsWith "?"
+
 let hey statement =
-    if statement = "Tom-ay-to, tom-aaaah-to." 
+
+    if endsWithQuestionMark statement
+        then "Sure."
+    elif statement = "Tom-ay-to, tom-aaaah-to." 
         then "Whatever."
-    elif statement = "Does this cryogenic chamber make me look fat?"
+    elif endsWithQuestionMark statement
         then "Sure."
     else    
         "Whoa, chill out!"
