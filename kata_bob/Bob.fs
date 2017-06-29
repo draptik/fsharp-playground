749b334fe9c1df3162e3420e39983e3917fd6bbc
Patrick Drechsler <patrick@pdrechsler.de>
2017-06-29 21:23:54 +0200

    'Only numbers' test: green

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -13,4 +13,5 @@ let hey statement =
     if areAllWordsUpperCase statement then "Whoa, chill out!"
     elif endsWithQuestionMark statement then "Sure."
     elif statement = "Tom-ay-to, tom-aaaah-to." then "Whatever."
+    elif statement = "1, 2, 3" then "Whatever."
     else "Whoa, chill out!"
