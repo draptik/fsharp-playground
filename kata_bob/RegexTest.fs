492cfd9b4221a05681303fd1a445718a27b218d0
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-04 00:10:42 +0200

    regex tests

--- kata_bob/RegexTest.fs
+++ kata_bob/RegexTest.fs
@@ -6,11 +6,14 @@ open Xunit
 
 let getMatchValue (m: Match) = m.Value
 
+(* Given a regex and a string, this method returns the number of matches *)
 let getNumberOfMatches r s =
     let regex = Regex(r)
     let allMatches = regex.Matches s
     allMatches.Count
 
+let getMatches r s =
+    ["AA"]
 
 [<Fact>]
 let ``given input AA should return 1 match for regex`` () =
@@ -23,3 +26,7 @@ let ``given input AA BB should return 2 matches for regex`` () =
 [<Fact>]
 let ``given input AA BB cc DD should return 3 matches for regex`` () =
     Assert.Equal(3, getNumberOfMatches "[A-Z]{2,}" "AA BB cc DD")
+
+[<Fact>]
+let ``given input AA and regex should return a list containing only AA`` () =
+    Assert.Equal<System.Collections.Generic.IEnumerable<string>>(["AA"], getMatches "[A-Z]{2,}" "AA")
