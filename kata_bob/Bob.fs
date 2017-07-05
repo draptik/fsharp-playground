193c512dea2760d882139f90022b2cc46e43b758
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-05 22:26:42 +0200

    refactored IsYelling and added some comments

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -3,20 +3,20 @@ module Bob
 open System
 open System.Text.RegularExpressions
 
-let areAllWordsUpperCaseAndNotAcronyms s =
-    let acronyms = ["OK"; "SOLID"]
+let isAnyWordUpperCaseAndNotAcronym s =
+    let acronyms = ["OK"]
     let m = Regex.Match(s, "[A-Z]{2,}") in
         if m.Success then
             let allMatchesAreAcronyms =
                 m.Groups
                 |> Seq.cast<Group>
-                |> Seq.map (fun x -> x.Value)
-                |> Seq.forall (fun x -> List.contains x acronyms)
+                |> Seq.map (fun x -> x.Value) (* 'map' corresponds to 'Select' in C# *)
+                |> Seq.forall (fun x -> List.contains x acronyms) (* 'forall' corresponds to 'All' in C#. Returns true if ALL uppercase words are acronyms *)
             not allMatchesAreAcronyms
         else false
 
 let (|IsYelling|_|) s =
-    if areAllWordsUpperCaseAndNotAcronyms s then Some IsYelling else None
+    if isAnyWordUpperCaseAndNotAcronym s then Some IsYelling else None
 
 let (|EndsWithQuestionMark|_|) (s:string) =
     if s.EndsWith "?" then Some EndsWithQuestionMark else None
