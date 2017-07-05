4a9d8eefba15bb7ced8e862d57199dba74010a26
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-05 21:29:22 +0200

    removed unused code

--- kata_bob/Bob.fs
+++ kata_bob/Bob.fs
@@ -3,54 +3,6 @@ module Bob
 open System
 open System.Text.RegularExpressions
 
-let (|UpperCase|) (x:string) = x.ToUpper()
-
-let (|Match|_|) pattern input =
-    let m = Regex.Match(input, pattern) in
-    if m.Success then Some (List.append [ for g in m.Groups -> g.Value ]) 
-    else None
-
-
-let rec fib n =
-    match n with
-    | 0 -> 0
-    | 1 -> 1
-    | _ -> fib (n - 1) + fib (n - 2)
-
-let (|AtLeast|_|) n score =
-    if score >= n then Some AtLeast else None
-
-let gradeMatch g =
-    match g with
-    | AtLeast 90 -> "A"
-    | AtLeast 80 -> "B"
-    | AtLeast 70 -> "C"
-    | AtLeast 60 -> "D"
-    | _ -> "F"
-
-let grade =
-    function
-    | AtLeast 90 -> "A"
-    | AtLeast 80 -> "B"
-    | AtLeast 70 -> "C"
-    | AtLeast 60 -> "D"
-    | _ -> "F"
-
-
-let getMatchValue (m: Match) = m.Value
-
-let matchInput s =
-    let r = Regex("[A-Z]{2,}")
-    let m = r.Match s in
-        if m.Success then
-            printfn "printline debugging: m.Groups = %A" m.Groups
-            m.Groups 
-            |> Seq.cast 
-            |> Seq.map getMatchValue 
-            |> Some
-        else    
-            None
-
 let areAllWordsUpperCaseAndNotAcronyms s =
     let acronyms = ["OK"; "SOLID"]
     let m = Regex.Match(s, "[A-Z]{2,}") in
