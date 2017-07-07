f2f87bb94391ce52e72d68927c647d7d8eddf1d0
Patrick Drechsler <patrick@pdrechsler.de>
2017-07-08 01:48:34 +0200

    moved repo content

---
+++ README.md
@@ -0,0 +1,3 @@
+Removed katas from exercism.io to new repo.
+
+I'll keep this repo for F# stuff.
--- kata_gigasecond/Gigasecond.fs
+++
@@ -1,5 +0,0 @@
-module Gigasecond
-open System
-    
-let gigasecond (d:DateTime) =
-    d.AddSeconds(1e9).Date
