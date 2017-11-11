5b33ea29af73ca07260880148d579df1e5b9a69c
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-11 17:40:05 +0100

    cleanup

--- scratch/DiamondTests.fs
+++ scratch/DiamondTests.fs
@@ -25,12 +25,8 @@ let dashes (num: int) =
 let getDiamond input =
     let upperLeft = 
         [ "A"; "B"; "C"]
-        |> List.mapi (fun index character -> dashes (2 - index) + character + dashes (index))
-    
-    // let upperLeft = 
-    //     [ dashes (2 - 0) + "A" + dashes (2 - 2); 
-    //       dashes (2 - 1) + "B" + dashes (2 - 1); 
-    //       dashes (2 - 2) + "C" + dashes (2 - 0)]
+        |> List.mapi (fun index character -> 
+            dashes (2 - index) + character + dashes (index))
 
     let upperHalf = 
         upperLeft
