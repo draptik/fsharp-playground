53a36fa38a58dc351ddc5196fc862e2f92932701
Patrick Drechsler <socialcoding@pdrechsler.de>
2017-12-12 12:30:17 +0100

    added demo4

---
+++ paket-demos/demo4/SomeApp/SomeLib2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace SomeLib2
+{
+    public class Class1
+    {
+        public void Foo()
+        {
+            var customer = new Customer { Name = "Homer" };
+            var result = JsonConvert.SerializeObject(customer);
+        }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+    }
+}
