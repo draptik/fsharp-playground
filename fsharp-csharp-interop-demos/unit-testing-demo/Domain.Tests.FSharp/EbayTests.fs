e680e5993c9a830111ec6c14cef62e80d14edaf1
Patrick Drechsler <patrick@pdrechsler.de>
2018-10-24 01:35:20 +0200

    fsunit is 10x slower than xunit's default assertion lib?

--- fsharp-csharp-interop-demos/unit-testing-demo/Domain.Tests.FSharp/EbayTests.fs
+++ fsharp-csharp-interop-demos/unit-testing-demo/Domain.Tests.FSharp/EbayTests.fs
@@ -4,9 +4,14 @@ open System
 open FsUnit.Xunit
 open Xunit
 open Domain
+open NHamcrest
 
 [<Fact>]
-let ``GetSearchResultFor returns correct result`` () =
+let ``GetSearchResultFor returns correct result (using FsUnit)`` () =
     // Calling a C# static method (from a static class) from F#:
     Ebay.GetSearchResultsFor("bar") |> should equal "BAR"
 
+
+[<Fact>]
+let ``GetSearchResultFor returns correct result (not using FsUnit)`` () =
+    Assert.That(Ebay.GetSearchResultsFor("bar"), Is.EqualTo("BAR"))
