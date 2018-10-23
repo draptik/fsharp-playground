d00eefdcc6a5ac114986c82695b1523917ef6bb4
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-10-24 00:18:09 +0200

    installed fsunit.xunit for f#

--- fsharp-csharp-interop-demos/unit-testing-demo/Domain.Tests.FSharp/Tests.fs
+++ fsharp-csharp-interop-demos/unit-testing-demo/Domain.Tests.FSharp/Tests.fs
@@ -1,8 +1,13 @@
 module Tests
 
+open FsUnit.Xunit
 open System
 open Xunit
 
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
+
+[<Fact>]
+let ``FsUnit works`` () =
+    true |> should equal true
