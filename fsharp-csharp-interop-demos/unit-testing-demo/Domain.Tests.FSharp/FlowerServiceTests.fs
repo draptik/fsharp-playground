004f019363eb8c66bb3c5efc22dd4629d90598f4
Patrick Drechsler <patrick@pdrechsler.de>
2018-10-24 02:22:53 +0200

    f# example template for dependencies (WIP)

---
+++ fsharp-csharp-interop-demos/unit-testing-demo/Domain.Tests.FSharp/FlowerServiceTests.fs
@@ -0,0 +1,13 @@
+module FlowerServiceTests
+
+open System
+open FsUnit.Xunit
+open Xunit
+open Domain
+open NHamcrest
+
+[<Fact>]
+let ``Send happy case calls deliveryService`` () =
+    // TODO
+    true
+
