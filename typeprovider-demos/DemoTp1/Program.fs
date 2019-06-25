07c3e9f336df0e5fdf06b23e22e572ab4b8be143
Patrick Drechsler <patrick@pdrechsler.de>
2019-06-25 21:44:26 +0200

    add typeprovider-demos

---
+++ typeprovider-demos/DemoTp1/Program.fs
@@ -0,0 +1,20 @@
+﻿open FSharp.Data
+
+// see https://fsharp.github.io/FSharp.Data/library/JsonProvider.html
+
+type Person = JsonProvider<"people.json">
+
+// Magic... This is type-safe!
+let printPeople =
+    let people = Person.GetSamples()
+    for person in people do
+        printf "%s " person.Name
+        person.Age |> Option.iter (printf "(%d)")
+        printfn ""
+    
+[<EntryPoint>]
+let main argv =
+
+    printPeople    
+
+    0 // return an integer exit code
---
+++ typeprovider-demos/DemoTp1/README.md
@@ -0,0 +1,3 @@
+# Simple F# Type Provider Demo
+
+Read a JSON file (`people.json`) and automatically have a type-safe representation of the data type on-the-fly.
