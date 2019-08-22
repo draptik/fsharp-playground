756b535ee18679b43c20a49b82fc0c0edb27b7ad
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-22 17:49:02 +0200

    applicative validation using fapply

--- user-with-applicative-validation/demo1/Domain.fs
+++ user-with-applicative-validation/demo1/Domain.fs
@@ -40,11 +40,9 @@ let createValidUser id firstName lastName dob twitter =
     
 let validateUser (unvalidatedUser : UnvalidatedUser) : Result<ValidatedUser, string list> =
     let v1 (u : UnvalidatedUser) =
-        let firstNameResult = FirstName.create u.FirstName
-        let lastNameResult = LastName.create u.LastName
         let f0 = createValidUser u.Id // partial application
-        let mapResult = Result.map f0 firstNameResult // functor map
-        let validationResult = apply mapResult lastNameResult // applicative apply
+        let mapResult = Result.map f0 (FirstName.create u.FirstName) // functor map
+        let validationResult = apply mapResult (LastName.create u.LastName) // applicative apply
         match validationResult with
         | Error err -> Error err
         | Ok f3 -> Ok (f3 u.Dob u.TwitterProfileUrl)
@@ -52,10 +50,8 @@ let validateUser (unvalidatedUser : UnvalidatedUser) : Result<ValidatedUser, str
 //    v1 unvalidatedUser
     
     let v2 (u : UnvalidatedUser) =
-        let firstNameResult = FirstName.create u.FirstName
-        let lastNameResult = LastName.create u.LastName
         let f0 = createValidUser u.Id
-        let validationResult = Result.map f0 firstNameResult <*> lastNameResult // infix apply
+        let validationResult = Result.map f0 (FirstName.create u.FirstName) <*> (LastName.create u.LastName) // infix apply
         match validationResult with
         | Error err -> Error err
         | Ok f3 -> Ok (f3 u.Dob u.TwitterProfileUrl)
@@ -63,10 +59,8 @@ let validateUser (unvalidatedUser : UnvalidatedUser) : Result<ValidatedUser, str
 //    v2 unvalidatedUser
 
     let v3 (u : UnvalidatedUser) =
-        let firstNameResult = FirstName.create u.FirstName
-        let lastNameResult = LastName.create u.LastName
         let f0 = createValidUser u.Id
-        let validationResult = f0 <!> firstNameResult <*> lastNameResult // infix map & apply
+        let validationResult = f0 <!> (FirstName.create u.FirstName) <*> (LastName.create u.LastName) // infix map & apply
         
         match validationResult with
         | Error err -> Error err
@@ -75,24 +69,18 @@ let validateUser (unvalidatedUser : UnvalidatedUser) : Result<ValidatedUser, str
 //    v3 unvalidatedUser
 
     let v4 (u : UnvalidatedUser) =
-        let firstNameResult = FirstName.create u.FirstName
-        let lastNameResult = LastName.create u.LastName
-        
-        let validationResult = (Ok (createValidUser u.Id))
-                               <*> firstNameResult
-                               <*> lastNameResult
-                               <*> (Ok u.Dob)
-                               <*> (Ok u.TwitterProfileUrl)
+        let validationResult = Ok (createValidUser u.Id)
+                               <*> FirstName.create u.FirstName
+                               <*> LastName.create u.LastName
+                               <*> Ok u.Dob
+                               <*> Ok u.TwitterProfileUrl
         validationResult
    
 //    v4 unvalidatedUser
 
     let v5 (u : UnvalidatedUser) =
-        let firstNameResult = FirstName.create u.FirstName
-        let lastNameResult = LastName.create u.LastName
-        
-        let x1 = apply (Ok (createValidUser u.Id)) firstNameResult
-        let x2 = apply x1 lastNameResult
+        let x1 = apply (Ok (createValidUser u.Id)) (FirstName.create u.FirstName)
+        let x2 = apply x1 (LastName.create u.LastName)
         let x3 = apply x2 (Ok u.Dob)
         let validationResult = apply x3 (Ok u.TwitterProfileUrl)
         validationResult
@@ -100,13 +88,10 @@ let validateUser (unvalidatedUser : UnvalidatedUser) : Result<ValidatedUser, str
 //    v5 unvalidatedUser
     
     let v6 (u : UnvalidatedUser) =
-        let firstNameResult = FirstName.create u.FirstName
-        let lastNameResult = LastName.create u.LastName
-        
         let flip f a b = f b a
             
-        let x1 = apply (Ok (createValidUser u.Id)) firstNameResult
-        let x2 = flip apply lastNameResult x1
+        let x1 = apply (Ok (createValidUser u.Id)) (FirstName.create u.FirstName)
+        let x2 = flip apply (LastName.create u.LastName) x1
         let x3 = flip apply (Ok u.Dob) x2
         let validationResult = flip apply (Ok u.TwitterProfileUrl) x3
         validationResult
@@ -114,14 +99,11 @@ let validateUser (unvalidatedUser : UnvalidatedUser) : Result<ValidatedUser, str
 //    v6 unvalidatedUser
     
     let v7 (u : UnvalidatedUser) =
-        let firstNameResult = FirstName.create u.FirstName
-        let lastNameResult = LastName.create u.LastName
-        
         let flip f a b = f b a
             
         let validationResult =
-            apply (Ok (createValidUser u.Id)) firstNameResult
-            |> flip apply lastNameResult
+            apply (Ok (createValidUser u.Id)) (FirstName.create u.FirstName)
+            |> flip apply (LastName.create u.LastName)
             |> flip apply (Ok u.Dob)
             |> flip apply (Ok u.TwitterProfileUrl)
 
@@ -130,16 +112,26 @@ let validateUser (unvalidatedUser : UnvalidatedUser) : Result<ValidatedUser, str
 //    v7 unvalidatedUser
 
     let v8 (u : UnvalidatedUser) =
-        let firstNameResult = FirstName.create u.FirstName
-        let lastNameResult = LastName.create u.LastName
-            
         let validationResult =
-            apply (Ok (createValidUser u.Id)) firstNameResult
-            |> fun x -> apply x lastNameResult
+            apply (Ok (createValidUser u.Id)) (FirstName.create u.FirstName)
+            |> fun x -> apply x (LastName.create u.LastName)
             |> fun x ->  apply x (Ok u.Dob)
             |> fun x -> apply x (Ok u.TwitterProfileUrl)
 
         validationResult
    
-    v8 unvalidatedUser
+//    v8 unvalidatedUser
+
+    let v9 (u : UnvalidatedUser) =
+        let fapply a b = apply b a
+        
+        let validationResult =
+            apply (Ok (createValidUser u.Id)) (FirstName.create u.FirstName)
+            |> fapply (LastName.create u.LastName)
+            |> fapply (Ok u.Dob)
+            |> fapply (Ok u.TwitterProfileUrl)
+
+        validationResult
+   
+    v9 unvalidatedUser
     
