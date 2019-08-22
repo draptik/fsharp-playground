b8128cbdcf3402edd2d1a99e0ceff0bc3606b6b9
Patrick Drechsler <patrick@pdrechsler.de>
2019-08-22 11:47:47 +0200

    user validation (firstname, lastname) using applicative

---
+++ user-with-applicative-validation/demo1/ResultExtensions.fs
@@ -0,0 +1,17 @@
+module demo1.ResultExtensions
+
+// infix map
+//let (<!>) = Result.map
+
+// Applicative validation =====================================================
+
+let apply (f : Result<'a -> 'b, 'c list>) (a : Result<'a, 'c list>) : Result<'b, 'c list> =
+    match f, a with
+    | Ok f, Ok a -> Ok(f a)
+    | Error f, Error a -> Error <| f @ a
+    | Error f, _ -> Error f
+    | _, Error a -> Error a
+
+let (<*>) = apply
+    
+let lift a = Ok a
